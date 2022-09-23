722ae36d7988f81e4622c8aa988f169a3ccb40e6
Zac Long <zac@192-168-1-114.tpgi.com.au>
2022-09-23 14:38:25 +1000

    initial user input to csv done

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import csv 
 
+
+
+
+print('Welcome to the Team Manager Assistant, before you get underway, there are several questions you must answer.')
+manager_name = input('What is your first name? ')
+namelist.append(manager_name)
+team_name = input('What is the name of your team? ')
+namelist.append(team_name)
+
+
+with open('nameteam.csv', 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow([manager_name, team_name])
+
+
 with open('nameteam.csv') as f:
     reader = csv.reader(f)
     reader.__next__()
     for row in reader:
-        print(f'Welcome back {row[0]}')
+        print(f'Welcome back {row[0]}, manager of {row[1]}')
