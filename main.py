328b6c0110cd7fec8e492d3c7798d8ecae251d5f
Zac Long <zac@Zacs-MacBook-Pro.local>
2022-09-23 18:45:56 +1000

    initiasation if statement done

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import csv 
+import csv_operations
 
 
 
-
-print('Welcome to the Team Manager Assistant, before you get underway, there are several questions you must answer.')
-manager_name = input('What is your first name? ')
-namelist.append(manager_name)
-team_name = input('What is the name of your team? ')
-namelist.append(team_name)
+if csv_operations.csv_filled() == False:
+    print('Welcome to the Team Manager Assistant, before you get underway, there are several questions you must answer.')
+    manager_name = input('What is your first name? ')
+    team_name = input('What is the name of your team? ')
 
 
-with open('nameteam.csv', 'a') as f:
-    writer = csv.writer(f)
-    writer.writerow([manager_name, team_name])
+    with open('nameteam.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['name', 'team'])
+        writer.writerow([manager_name, team_name])
 
 
 with open('nameteam.csv') as f:
     reader = csv.reader(f)
     reader.__next__()
     for row in reader:
-        print(f'Welcome back {row[0]}, manager of {row[1]}')
+        print(f'Welcome {row[0]}, manager of {row[1]}')
+        break
