fd05e7ff19f6a72f3eccd68f1a60d185402f4bd1
Zac Long <zac@Zacs-MBP.modem>
2022-09-28 11:41:49 +1000

    cleaned up the view roster function

--- view_roster.py
+++ view_roster.py
@@ -1,6 +1,7 @@
 import csv
 import csv_operations as co
 from prettytable import from_csv
+import pandas as pd
 
 def display_roster():
     with open('roster.csv', 'r') as f:
@@ -9,7 +10,13 @@ def display_roster():
 
 
 def roster_check():
-    if co.csv_filled('roster.csv') == False:
+    rowcount = 0
+
+    for row in open('roster.csv'):
+        rowcount += 1
+    
+    if rowcount <= 1:
         print('You need to add players before you can view the roster! ')
+        return
     else:
         display_roster()
