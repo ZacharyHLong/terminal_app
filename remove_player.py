27599cbc5ed5d128559616dccc6632ad862bed05
Zac Long <zac@Zacs-MacBook-Pro.local>
2022-10-01 12:32:34 +1000

    fixed remove player loop, to actually loop

--- remove_player.py
+++ remove_player.py
@@ -8,23 +8,22 @@ def remove_loop():
     cont_loop = True
     roster = pd.read_csv('roster.csv', index_col ='First Name')
     vr.roster_check('remove players from')
+    
     remove_player_loop = input('Do you want to remove a player from the roster? (y/n): ')
-
-    if remove_player_loop == 'n':
-        cont_loop = False
-        return
-
-    elif remove_player_loop == 'y':
+# ccc
+    if remove_player_loop == 'y':
         while cont_loop == True:     
             user_input = ex.removal_checker('Enter the first name of the player you would like to remove?: ')
-            # needs name checker here!!!
             drop_confirmation = input('Are you sure you want to remove ' + user_input + '? (y/n): ')
 
             if drop_confirmation == 'y':
                 roster.drop(user_input, axis = 0, inplace = True)
                 roster.to_csv('roster.csv')
                 print('Successfully removed ' + user_input)
-                cont_loop = False
+                loop_break = input('Would you like to remove another player? (y/n): ')
+                if loop_break == 'n':
+                    cont_loop = False
+                    break
 
             elif drop_confirmation == 'n':
                 cont_loop = False
@@ -32,6 +31,12 @@ def remove_loop():
 
             else:
                 print('Please enter either y or n. ')
+
+    elif remove_player_loop == 'n':
+        cont_loop = False
+        return
+
+        
     else:
         print('Please enter either y or n. ')
 
